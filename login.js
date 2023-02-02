434cd890d85d54ec3c83d0b80de37d0cf0ff0f8e
0andme <yu00mi97@gmail.com>
2023-02-02 13:49:44 +0900

    #1 feat : 로그인 관련 함수 추가
    - 로그인 api 호출 함수
    - 로그인 버튼 핸들링 함수

---
+++ login.js
@@ -0,0 +1,24 @@
+async function login(user) {
+  let res = await fetch(`${SERVER_URL}/user/login`, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json",
+    },
+    credentials: "include",
+  });
+  let data = await res.json();
+  return data;
+}
+
+async function submitLogin() {
+  let user = {
+    email: document.getElementById("userEmail").value,
+    password: document.getElementById("userPassword").value,
+  };
+  let res = await login(user);
+  console.log(res);
+  if (res.access_token) {
+    setCookie("access_token", res.access_token);
+  }
+}
