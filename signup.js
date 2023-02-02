d989fb860bed19546b84ab41b66cbc2e4d84bdd8
0andme <yu00mi97@gmail.com>
2023-02-02 13:48:45 +0900

    #2 feat : 회원가입 관련 함수 추가
    - 회원가입 api호출 함수
    - 회원가입 버튼 핸들링 함수

---
+++ signup.js
@@ -0,0 +1,20 @@
+async function register(user) {
+  let res = await fetch(`${SERVER_URL}/user/register`, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json",
+    },
+    credentials: "include",
+  });
+}
+
+async function submitRegister() {
+  let user = {
+    email: document.getElementById("userEmail").value,
+    password: document.getElementById("userPassword").value,
+    fullname: document.getElementById("userName").value,
+  };
+
+  let res = await register(user);
+}
